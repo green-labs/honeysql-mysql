2870bca3cd2c49666426141f71a52307b7c27532
SongKJ00 <gowithdsm@gmail.com>
2022-08-26 01:33:50 +0900

    Add format ns

--- src/honeysql_mysql/helpers.clj
+++ src/honeysql_mysql/helpers.clj
@@ -1,5 +1,6 @@
 (ns honeysql-mysql.helpers
-  (:require [honeysql.helpers :as h :refer [defhelper]]))
+  (:require [honeysql-mysql.format]
+            [honeysql.helpers :as h :refer [defhelper]]))
 
 (defhelper insert-ignore-into [m fields]
   (assoc m :insert-ignore-into (first fields)))
