632e3636c413681778c510d21aca794056190ed4
SongKJ00 <gowithdsm@gmail.com>
2022-08-26 14:48:18 +0900

    Use honeysql v2 functions

--- src/honeysql_mysql/format.clj
+++ src/honeysql_mysql/format.clj
@@ -1,31 +1,27 @@
 (ns honeysql-mysql.format
-  (:require [honeysql.format :as fmt]
-            [honeysql.core :as sql]
-            [honeysql.helpers :as h]
-            [honeysql-mysql.helpers :as mysqlh]))
+  (:require [clojure.string :as string]
+            [camel-snake-kebab.core :as csk]
+            [honey.sql :as sql]
+            [honey.sql.helpers :as h]))
 
-(def custom-clause-priorities
-  {:insert-ignore-into (:insert-into fmt/default-clause-priorities)})
+(defn insert-ignore-into-formatter
+  [_op args]
+  (let [insert-into (if (vector? args)
+                      (apply h/insert-into args)
+                      (h/insert-into args))
+        insert-into' (-> insert-into
+                         sql/format
+                         first)
+        result (string/replace insert-into' #"INSERT" "INSERT IGNORE")]
+    [result]))
+
+(def custom-clauses
+  {:insert-ignore-into {:formatter insert-ignore-into-formatter
+                        :before    :columns}})
 
 (defn register-custom-clauses!
   []
-  (doseq [[clause-key priority] custom-clause-priorities]
-    (fmt/register-clause! clause-key priority)))
-
-(defmethod fmt/format-clause :insert-ignore-into [[_op table-name] _sqlmap]
-  (str "INSERT IGNORE INTO " (fmt/to-sql table-name)))
+  (doseq [[clause {:keys [formatter before]}] custom-clauses]
+    (sql/register-clause! clause formatter before)))
 
 (register-custom-clauses!)
-
-(comment
-  custom-clause-priorities
-
-  (sql/format {:insert-ignore-into :some-table
-               :columns            [:col1 :col2]
-               :values             [[1 2] [3 4]]})
-  (sql/format (-> (mysqlh/insert-ignore-into :some-table)
-                  (h/columns :col1 :col2)
-                  (h/values [[1 2] [3 4]])))
-  ;; => ["INSERT IGNORE INTO some_table (col1, col2) VALUES (?, ?), (?, ?)" 1 2 3 4]
-  )
-
