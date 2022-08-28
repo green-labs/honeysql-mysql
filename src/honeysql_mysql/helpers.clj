bff518e8104b21393428092a17c7469050d2db01
Hyunwoo Nam <namenu@gmail.com>
2022-08-29 00:25:18 +0900

    match-against 초기 구현 추가 (#3)
    * match-against 초기 구현 추가
    
    * add tests

--- src/honeysql_mysql/helpers.clj
+++ src/honeysql_mysql/helpers.clj
@@ -1,7 +1,5 @@
 (ns honeysql-mysql.helpers
-  (:require [honeysql-mysql.format :as fmt]
-            [honey.sql :as sql]
-            [honey.sql.helpers :as h]))
+  (:require [honey.sql.helpers :as h]))
 
 (defn insert-ignore-into
   [& args]
--- test/honeysql_mysql/mysql_test.clj
+++ test/honeysql_mysql/mysql_test.clj
@@ -1,8 +1,13 @@
 (ns honeysql-mysql.mysql-test
   (:require [clojure.test :refer [deftest run-tests is testing]]
+            [honeysql-mysql.format :refer [extend-syntax!]]
             [honeysql-mysql.helpers :as mysql-h]
             [honey.sql :as sql]
             [honey.sql.helpers :as h]))
+
+(clojure.test/use-fixtures :once (fn [f]
+                                   (extend-syntax!)
+                                   (f)))
 
 (deftest insert-ignore-into-test
   (testing "INSERT IGNORE INTO sql generation for mysql"
@@ -16,9 +21,30 @@
                (h/values [{:col1 1 :col2 2} {:col1 3 :col2 4}])
                sql/format)))))
 
+(deftest match-against-test
+  (testing "search-mode"
+    (is (= ["WHERE MATCH (contents) AGAINST ('검색어1 검색어2')"]
+           (sql/format
+             {:where  [:match-against [:contents] "검색어1 검색어2"]}
+             {:inline true})))
+    (is (= ["WHERE MATCH (contents) AGAINST ('검색어1 검색어2' IN BOOLEAN MODE)"]
+           (sql/format
+             {:where  [:match-against [:contents] "검색어1 검색어2" :in-boolean-mode]}
+             {:inline true}))))
+
+  (testing "in where"
+    (let [orderer-name "john"]
+      (is (= ["WHERE MATCH (orderer_name) AGAINST (? IN BOOLEAN MODE)" "john"]
+             (sql/format {:where [:match-against [:orderer_name] orderer-name :in-boolean-mode]})))))
+
+  (testing "in select"
+    (let [text "Security implications of running MySQL as root"
+          mode :in-natural-language-mode]
+      (is (= ["SELECT id, body, MATCH (title, body) AGAINST ('Security implications of running MySQL as root' IN NATURAL LANGUAGE MODE) AS score FROM articles WHERE (MATCH (title, body) AGAINST ('Security implications of running MySQL as root' IN NATURAL LANGUAGE MODE))"])
+          (sql/format {:select [:id :body [[:match-against [:title :body] text mode] :score]]
+                       :from   :articles
+                       :where  [:match-against [:title :body] text mode]}
+                      {:inline true})))))
+
 (comment
   (run-tests))
-
-
-
-
