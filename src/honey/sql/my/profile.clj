4b72e4db75f6979f02cf7fa27d5a8dfdd9a989af
SongKJ00 <gowithdsm@gmail.com>
2022-09-06 14:04:09 +0900

    Add ns

--- src/honey/sql/my/profile.clj
+++ src/honey/sql/my/profile.clj
@@ -1,5 +1,6 @@
 (ns honey.sql.my.profile
-  (:require [honey.sql :as sql]))
+  (:require [honey.sql :as sql]
+            [honey.sql.my.format :as format]))
 
 (defn format [data opts]
   (tap> (honey.sql/format (merge data {:explain []}) opts))
